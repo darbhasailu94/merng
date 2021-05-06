d80017a8e4d1b52e3fdcdb3049eb70c2158cf8dc
Sailendra Darbha <darbhasailu@gmail.com>
2021-05-07 01:41:11 +0530

    mutation for create post added, auth headers validation added, server working as expected

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,11 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth.js');
 
 module.exports = {
     Query: {
         async getPosts(){
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({createdAt: -1});
                 return posts;
             } catch(err) {
                 throw new Error(err);
@@ -22,6 +23,21 @@ module.exports = {
             catch(err){
                 throw new Error(err);
             }
+        }
+    },
+    Mutation: {
+        async createPost(_,{body},context){
+            const user = checkAuth(context);
+            const newPost = new Post({
+                body,
+                user:user.id,
+                username:user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
         }
     }
 };
