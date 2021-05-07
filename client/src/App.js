ec7641be26fb3406eaedc98739b9f3b6efada291
Sailendra Darbha <darbhasailu@gmail.com>
2021-05-08 02:52:36 +0530

    sematic ui&css packages added, react-dom-render package added, pages/routes for home,login and register added, menubar component created, routes functional and server working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+import { Container } from 'semantic-ui-react';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
+  // the Container component basically means <div class="ui container"></div>
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Container>
+    </Router>
   );
 }
 
